387434604af441c282613dfda4569ae5b653d247
Inal Gotov <sketch204@gmail.com>
2023-11-10 15:04:16 -0500

    Initial commit

---
+++ AtomReader/AtomReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtomReaderApp.swift
+//  AtomReader
+//
+//  Created by Inal Gotov on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct AtomReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
