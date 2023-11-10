5067a50722e05297b7149052d47a94601be88edb
Inal Gotov <sketch204@gmail.com>
2023-11-10 15:16:10 -0500

    Add basic parsing

--- AtomReader/ContentView.swift
+++ AtomReader/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import AtomParser
 
 struct ContentView: View {
+    @State private var url: String = ""
+    @State private var feed: Feed?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("URL", text: $url)
+            
+            Text(feed != nil ? "Parsed": "Parsing")
+            
+            Button("Submit", action: submitForm)
         }
-        .padding()
+    }
+    
+    private func submitForm() {
+        guard let url = URL(string: url) else { return }
+        
+        Task.detached(priority: .userInitiated) {
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                
+//                print(String(data: data, encoding: .utf8)!)
+                
+                feed = try Feed(data: data)
+            } catch {
+                print("ERROR: \(error)")
+            }
+        }
     }
 }
 
