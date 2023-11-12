9968f9b96b40fdd497f789731c61723341d4b1e5
Inal Gotov <sketch204@gmail.com>
2023-11-12 15:19:03 -0500

    Add preview handlers for `AppActions`

---
+++ AtomReader/Preview Content/AppActions+Preview.swift
@@ -0,0 +1,14 @@
+//
+//  AppActions+Preview.swift
+//  AtomReader
+//
+//  Created by Inal Gotov on 2023-11-12.
+//
+
+import SwiftUI
+
+extension View {
+    func previewAppActions() -> some View {
+        environment(AppActions())
+    }
+}
