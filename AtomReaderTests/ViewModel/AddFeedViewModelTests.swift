b3bdb02507860ee4a811e9eea2cec0c2396939d0
Inal Gotov <sketch204@gmail.com>
2023-12-02 21:53:21 -0500

    Add more tests for `AddFeedViewModel`

--- AtomReaderTests/ViewModel/AddFeedViewModelTests.swift
+++ AtomReaderTests/ViewModel/AddFeedViewModelTests.swift
@@ -19,8 +19,11 @@ final class AddFeedViewModelTests: XCTestCase {
             feedPreviewer: makeTestFeedPreviewer()
         )
     }
-    
-    func test_previewFeedFetching_whenFullFeedUrlSet_fetchesPreviews() async {
+}
+
+
+extension AddFeedViewModelTests {
+    func test_feedUrlString_whenFullFeedUrlSet_fetchesPreviews() async {
         sut.feedUrlString = "https://hello.mock/feed"
         
         try? await Task.sleep(for: .milliseconds(400))
@@ -28,7 +31,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.feedPreviews, [mockFeed1])
     }
     
-    func test_previewFeedFetching_whenPartialFeedUrlSet_fetchesPreviews() async {
+    func test_feedUrlString_whenPartialFeedUrlSet_fetchesPreviews() async {
         sut.feedUrlString = "hello.mock/feed"
         
         try? await Task.sleep(for: .milliseconds(400))
@@ -36,7 +39,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.feedPreviews, [mockFeed1])
     }
     
-    func test_previewFeedFetching_whenFullWebsiteUrlSet_fetchesPreviews() async {
+    func test_feedUrlString_whenFullWebsiteUrlSet_fetchesPreviews() async {
         sut.feedUrlString = "https://hello.mock"
         
         try? await Task.sleep(for: .milliseconds(400))
@@ -44,7 +47,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.feedPreviews, [mockFeed1])
     }
     
-    func test_previewFeedFetching_whenPartialWebsiteUrlSet_fetchesPreviews() async {
+    func test_feedUrlString_whenPartialWebsiteUrlSet_fetchesPreviews() async {
         sut.feedUrlString = "hello.mock"
         
         try? await Task.sleep(for: .milliseconds(400))
@@ -52,7 +55,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.feedPreviews, [mockFeed1])
     }
     
-    func test_previewFeedFetching_whenEmptyUrlSet_ignoresIt() async {
+    func test_feedUrlString_whenEmptyUrlSet_ignoresIt() async {
         let sut = AddFeedViewModel(
             store: store,
             feedPreviewer: FeedPreviewer(
@@ -68,7 +71,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertFalse(sut.isLoading)
     }
     
-    func test_previewFeedFetching_rightAfterChangingPreviews_resetFeeds() async {
+    func test_feedUrlString_rightAfterChangingPreviews_resetFeeds() async {
         let sut = AddFeedViewModel(
             store: store,
             feedPreviewer: FeedPreviewer(
@@ -89,7 +92,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.selectedFeeds, [])
     }
     
-    func test_previewFeedFetching_whenFetching_setIsLoadingCorrectly() async {
+    func test_feedUrlString_whenFetchingFeed_selectedFeedsAutoSet() async {
         let sut = AddFeedViewModel(
             store: store,
             feedPreviewer: FeedPreviewer(
@@ -98,47 +101,14 @@ final class AddFeedViewModelTests: XCTestCase {
             )
         )
         
-        XCTAssertFalse(sut.isLoading)
-        
-        sut.feedUrlString = "https://hello.mock/feed"
-        
-        try? await Task.sleep(for: .milliseconds(250))
-        
-        XCTAssertTrue(sut.isLoading)
-        
-        try? await Task.sleep(for: .milliseconds(400))
-        
-        XCTAssertFalse(sut.isLoading)
-    }
-    
-    func test_isFeedAlreadyAdded_whenNewFeedProvided_returnsFalse() {
-        let isAdded = sut.isFeedAlreadyAdded(mockFeed1)
-        XCTAssertFalse(isAdded)
-    }
-    
-    func test_isFeedAlreadyAdded_whenExistingFeedProvided_returnsTrue() {
-        let isAdded = sut.isFeedAlreadyAdded(mockFeed2)
-        XCTAssertTrue(isAdded)
-    }
-    
-    func test_addFeeds_whenFeedsSelected_addsFeeds() async {
         sut.feedUrlString = mockFeed1.feedUrl.absoluteString
         
-        try? await Task.sleep(for: .milliseconds(500))
+        try? await Task.sleep(for: .milliseconds(600))
         
-        sut.selectedFeeds = [mockFeed1.id]
-        sut.addFeeds()
-        
-        XCTAssertEqual(store.feeds, [mockFeed2, mockFeed1])
+        XCTAssertEqual(sut.selectedFeeds, [mockFeed1.id])
     }
     
-    func test_addFeeds_whenNoFeedsSelected_noChangesMade() {
-        sut.addFeeds()
-        
-        XCTAssertEqual(store.feeds, [mockFeed2])
-    }
-    
-    func test_selectedFeeds_afterChangingPreviews_resetSelectedFeeds() async {
+    func test_feedUrlString_afterChangingPreviews_resetSelectedFeeds() async {
         sut.feedUrlString = mockFeed1.feedUrl.absoluteString
         
         try? await Task.sleep(for: .milliseconds(400))
@@ -152,7 +122,7 @@ final class AddFeedViewModelTests: XCTestCase {
         XCTAssertEqual(sut.selectedFeeds, [mockFeed3.id])
     }
     
-    func test_previewFeed_whenErrorEncountered_errorMessageSet() async {
+    func test_feedUrlString_whenErrorEncountered_errorMessageSet() async {
         struct CustomError: Error, CustomDebugStringConvertible {
             let message: String = "Unsupported"
             var localizedDescription: String { debugDescription }
@@ -178,5 +148,214 @@ final class AddFeedViewModelTests: XCTestCase {
         try? await Task.sleep(for: .milliseconds(600))
         
         XCTAssertEqual(sut.errorMessage, CustomError().message)
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_isLoading_whenFetching_isLoadingSetCorrectly() async {
+        let sut = AddFeedViewModel(
+            store: store,
+            feedPreviewer: FeedPreviewer(
+                feedProvider: DelayedDataProvider(),
+                networkInterface: MockNetworkInterface()
+            )
+        )
+        
+        XCTAssertFalse(sut.isLoading)
+        
+        sut.feedUrlString = mockFeed1.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(250))
+        
+        XCTAssertTrue(sut.isLoading)
+        
+        try? await Task.sleep(for: .milliseconds(400))
+        
+        XCTAssertFalse(sut.isLoading)
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_canAddFeed_whenNoPreviewsFetched_false() async {
+        let sut = AddFeedViewModel(
+            store: store,
+            feedPreviewer: FeedPreviewer(
+                feedProvider: DelayedDataProvider(),
+                networkInterface: MockNetworkInterface()
+            )
+        )
+        
+        XCTAssertFalse(sut.canAddFeed)
+    }
+    
+    func test_canAddFeed_whenPreviewsFetchedButNothingSelected_false() async {
+        let sut = AddFeedViewModel(
+            store: store,
+            feedPreviewer: FeedPreviewer(
+                feedProvider: DelayedDataProvider(),
+                networkInterface: MockNetworkInterface()
+            )
+        )
+        
+        sut.feedUrlString = mockFeed1.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(600))
+        
+        sut.selectedFeeds = []
+        
+        XCTAssertFalse(sut.canAddFeed)
+    }
+    
+    func test_canAddFeed_whenPreviewsFetchedButSelectionIsAlreadyAdded_false() async {
+        let sut = AddFeedViewModel(
+            store: store,
+            feedPreviewer: FeedPreviewer(
+                feedProvider: DelayedDataProvider(),
+                networkInterface: MockNetworkInterface()
+            )
+        )
+        
+        sut.feedUrlString = mockFeed2.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(600))
+        
+        XCTAssertFalse(sut.canAddFeed)
+    }
+    
+    func test_canAddFeed_whenPreviewsFetchedAndSelected_true() async {
+        let sut = AddFeedViewModel(
+            store: store,
+            feedPreviewer: FeedPreviewer(
+                feedProvider: DelayedDataProvider(),
+                networkInterface: MockNetworkInterface()
+            )
+        )
+        
+        sut.feedUrlString = mockFeed1.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(600))
+        
+        XCTAssertTrue(sut.canAddFeed)
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_isFeedAlreadyAdded_whenNewFeedProvided_returnsFalse() {
+        let isAdded = sut.isFeedAlreadyAdded(mockFeed1)
+        XCTAssertFalse(isAdded)
+    }
+    
+    func test_isFeedAlreadyAdded_whenExistingFeedProvided_returnsTrue() {
+        let isAdded = sut.isFeedAlreadyAdded(mockFeed2)
+        XCTAssertTrue(isAdded)
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_addFeeds_whenFeedsSelected_addsFeeds() async {
+        sut.feedUrlString = mockFeed1.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(500))
+        
+        sut.selectedFeeds = [mockFeed1.id]
+        sut.addFeeds()
+        
+        XCTAssertEqual(store.feeds, [mockFeed2, mockFeed1])
+    }
+    
+    func test_addFeeds_whenNoFeedsSelected_noChangesMade() {
+        sut.addFeeds()
+        
+        XCTAssertEqual(store.feeds, [mockFeed2])
+    }
+    
+    func test_addFeeds_whenNonExistedFeedSelected_ignoresIt() async {
+        sut.feedUrlString = mockFeed1.feedUrl.absoluteString
+        
+        try? await Task.sleep(for: .milliseconds(500))
+        
+        sut.setFeed(mockFeed3, selected: true)
+        
+        sut.addFeeds()
+        
+        XCTAssertEqual(store.feeds, [mockFeed2, mockFeed1])
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_isFeedSelected_whenNonExistedFeedPrompted_false() {
+        sut.selectedFeeds = []
+        
+        XCTAssertFalse(sut.isFeedSelected(mockFeed1))
+    }
+    
+    func test_isFeedSelected_whenNonSelectedFeedPrompted_false() {
+        sut.selectedFeeds = [mockFeed2.id]
+        
+        XCTAssertFalse(sut.isFeedSelected(mockFeed1))
+    }
+    
+    func test_isFeedSelected_whenSelectedFeedPrompted_true() {
+        sut.selectedFeeds = [mockFeed1.id]
+        
+        XCTAssertTrue(sut.isFeedSelected(mockFeed1))
+    }
+}
+
+
+extension AddFeedViewModelTests {
+    func test_toggleFeedSelection_whenNonSelectedFeedPassed_makesItSelected() {
+        sut.selectedFeeds = []
+        
+        sut.toggleFeedSelection(mockFeed1)
+        
+        XCTAssertEqual(sut.selectedFeeds, [mockFeed1.id])
+    }
+    
+    func test_toggleFeedSelection_whenSelectedFeedPassed_makesItNotSelected() {
+        sut.selectedFeeds = [mockFeed1.id]
+        
+        sut.toggleFeedSelection(mockFeed1)
+        
+        XCTAssertEqual(sut.selectedFeeds, [])
+    }
+}
+
+extension AddFeedViewModelTests {
+    func test_setFeedSelected_whenSelectingNonSelected_makesItSelected() {
+        sut.selectedFeeds = []
+        
+        sut.setFeed(mockFeed1, selected: true)
+        
+        XCTAssertEqual(sut.selectedFeeds, [mockFeed1.id])
+    }
+    
+    func test_setFeedSelected_whenUnselectingNonSelected_() {
+        sut.selectedFeeds = []
+        
+        sut.setFeed(mockFeed1, selected: false)
+        
+        XCTAssertEqual(sut.selectedFeeds, [])
+    }
+    
+    func test_setFeedSelected_whenSelectingSelected_keepsItNotUnselected() {
+        sut.selectedFeeds = [mockFeed1.id]
+        
+        sut.setFeed(mockFeed1, selected: true)
+        
+        XCTAssertEqual(sut.selectedFeeds, [mockFeed1.id])
+    }
+    
+    func test_setFeedSelected_whenUnselectingSelected_makesItUnselected() {
+        sut.selectedFeeds = [mockFeed1.id]
+        
+        sut.setFeed(mockFeed1, selected: false)
+        
+        XCTAssertEqual(sut.selectedFeeds, [])
     }
 }
